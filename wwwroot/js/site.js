97a10a267ee62d46c146a1486a6ab5ba84e6250a
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-04-30 01:41:44 +0000

    Tesst

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Write your JavaScript code.
+console.log("Javascript up and running");
+
+$(function() {
+    // Setup drop down menu
+    $('.dropdown-toggle').dropdown();
+   
+    // Fix input element click problem
+    $('.dropdown input, .dropdown label').click(function(e) {
+      e.stopPropagation();
+    });
+  });
