d07863600af3c9729f0713ace69961d9707e93e6
Vidir Snaer Svanbjornsson <vidir17@ru.is>
2018-05-03 14:21:36 +0000

    UserAccount entitymodel
    .

---
+++ Data/EntityModels/UserAccount.cs
@@ -0,0 +1,16 @@
+namespace BookCave.Data.EntityModels
+{
+    using System.Collections.Generic;
+    public class UserAccount
+    {
+        public string userID {get; set;}
+        public string userName {get; set;}
+        public string password {get; set;}
+        public Book favoriteBook {get; set;}
+        public string picture {get; set;}
+        public CardInfo cardInfo {get; set;}
+        public ShippingInfo shippingInfo {get; set;}
+        public List<Order> orderHistory {get; set;}
+        public List<Book> wishList {get; set;}
+    }
+}
