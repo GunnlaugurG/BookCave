92a1b03d9a8fa8500409acfad2e2f2a7f39a43ec
Elías Nökkvi Gíslason <eliasnokkvi@yahoo.com>
2018-05-04 19:13:43 +0000

    Database is now running! woowoo

--- Services/BookServices.cs
+++ Services/BookServices.cs
@@ -10,17 +10,39 @@ namespace BookCave.Services {
     public class BookServices {
 
         private BookRepo _bookRepo;
-
         public BookServices(){
             _bookRepo = new BookRepo();
-
         }
         public List<BookListViewModel> GetAllBooks(){
             
             return _bookRepo.GetAllBooks();
-
         }
-
+        public List<BookListViewModel> GetTopTenBooks(int value) {
+            
+            if(value == 1) {
+            var topRating = (from b in _bookRepo.GetAllBooks()
+                           orderby b.rating descending
+                           select b).Take(10).ToList();
+                           return topRating;
+            }
+            if(value == 0) {
+            var topPrice = (from b in _bookRepo.GetAllBooks()
+                           orderby b.cost descending
+                           select b).Take(10).ToList();
+                           return topPrice;
+            }
+            if(value == 3) {
+            var topName = (from b in _bookRepo.GetAllBooks()
+                           orderby b.title
+                           select b).Take(10).ToList();
+                           return topName;
+            }
+            else {
+                var topBooks = (from b in _bookRepo.GetAllBooks()
+                           orderby b.rating descending
+                           select b).Take(10).ToList();
+                           return topBooks;
+                }
+            }
+        }
     }
-
-}
