664c86d4d9068e1169ad3cc09e1c8503f443d2c6
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-05-04 16:28:34 +0000

    pulling
    asdf

--- Data/EntityModels/UserAccount.cs
+++ Data/EntityModels/UserAccount.cs
@@ -6,11 +6,11 @@ namespace BookCave.Data.EntityModels
         public string userID {get; set;}
         public string userName {get; set;}
         public string password {get; set;}
-        public string favoriteBookId {get; set;}
+        //public Book favoriteBook {get; set;}
         public string picture {get; set;}
         public CardInfo cardInfo {get; set;}
         public ShippingInfo shippingInfo {get; set;}
         public List<Order> orderHistory {get; set;}
-        public List<BookId> wishList {get; set;}
+        //public List<Book> wishList {get; set;}
     }
 }
