35a7cff61efe3278c4bfff69b4a852f3316081f9
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-05-04 17:17:43 +0000

    newChanges
    adf

---
+++ Migrations/20180504170634_bookAdded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class bookAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    author = table.Column<string>(nullable: true),
+                    cost = table.Column<double>(nullable: false),
+                    description = table.Column<string>(nullable: true),
+                    genre = table.Column<string>(nullable: true),
+                    image = table.Column<string>(nullable: true),
+                    keyAuthorId = table.Column<string>(nullable: true),
+                    rating = table.Column<double>(nullable: false),
+                    title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -29,17 +29,10 @@ namespace BookCave
         public static void SeedData() {
 
             var db = new DataContext();
-            var authors = new List<Author>() {
-             new Author {
-                    authorName = "Salman Rushdie",
-                    image = "http://www.notablebiographies.com/images/uewb_09_img0619.jpg"
-                },
-             new Author {
-                    authorName = "F. Scott Fitzgerald",
-                    image = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/F_Scott_Fitzgerald_1921.jpg/440px-F_Scott_Fitzgerald_1921.jpg"
-                }
+            var books = new List<Book>() {
+
             };
-            db.AddRange(authors);
+            db.AddRange(books);
             db.SaveChanges();
         }
     }
