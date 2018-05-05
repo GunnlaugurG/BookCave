b601fc3c864f3b4160924ad3099d2a128c245a5d
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-05-05 14:37:47 +0000

    asdf
    asdf

---
+++ Models/ViewModels/AccountDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookCave.Models.ViewModels
+{
+    public class AccountDetailsViewModel
+    {
+        public string userName { get; set; }
+        public byte[] picture { get; set; }
+    }
+}
