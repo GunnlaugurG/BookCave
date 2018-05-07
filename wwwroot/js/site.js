0326d43b50a81f5c2cb6812884e3b7a4ab3c7815
Stefanorn <stefanorn92@gmail.com>
2018-05-07 10:55:31 +0000

    Lét top books leitina loda með Ajax

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,44 @@
 ﻿// Write your JavaScript code.
-console.log("Javascript up and running");
+console.log("Javascript up and running1");
+
+
+$("#sort-by").change(function(){
+    var sortVal = $(this).val();
+    $.post("SortBy", { value: sortVal },
+    function(data, status){
+      console.log(data); 
+      var markup = "";
+      for( var i = 0; i < data.length; i++){
+        markup += '<div id="settings" class="col-lg-3 col-sm-6 col-md-4">';
+        markup += '<div class="row">';
+        markup += '<div class="col-lg-12 col-sm-12">';
+        markup += ' <img class="Thumb-nail-image" src="' + data[i].image + '" alt="book-cover" />';
+        markup += '</div>';
+        markup += '</div>';
+        markup += '<div class="row">';
+        markup += '<div id="book-info" class="col-lg-12 col-sm-12">';
+        markup += '<a href="details/' + data[i].id + '" class="card-text">' + data[i].title + '</a>';
+        markup += '<div class="divider"></div>';
+        markup += '<a href="/author/details/' + data[i].author.id + '">' + data[i].author.authorName + '</a>' ;
+        markup += '<p>Rating: ' + data[i].rating + '</p>';
+        markup += '<p>Price: ' + data[i].cost + ' $</p>';
+        markup += '</div>';
+        markup += '</div>';
+        markup += '<div class="row">';
+        markup += '<div class="col-lg-12 col-sm-12 img-thumbnail-btn">';
+        markup += '<a class="btn btn-success center-block" href="#">Add to cart</a>';
+        markup += '</div>';
+        markup += '</div>';
+        markup += '<div class="row">';
+        markup += '<div class="col-lg-12 col-sm-4 spacing">';
+        markup += '</div>';
+        markup += '</div>';
+        markup += '</div>';
+      }
+      $("#book-list").empty();
+      $("#book-list").append(markup);
+    })
+});
 
 $(function() {
     // Setup drop down menu
@@ -10,6 +49,4 @@ $(function() {
       e.stopPropagation();
     });
   });
-
-
 
