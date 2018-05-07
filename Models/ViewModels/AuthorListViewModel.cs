aa21815030333095a36b4168dbb5977223347db0
Elías Nökkvi Gíslason <eliasnokkvi@yahoo.com>
2018-05-07 15:14:04 +0000

    d

--- Models/ViewModels/AuthorListViewModel.cs
+++ Models/ViewModels/AuthorListViewModel.cs
@@ -5,6 +5,7 @@ namespace BookCave.Models.ViewModels {
     public class AuthorListViewModel {
              public string Id { get; set; }
              public string authorName { get; set; }
+             public List<Book> writtenBooks { get; set; }
              public Book mostPopularBook { get; set; }
     }
 }
