cac36eb58de7ed8a62650533499d6b8c23d7fc37
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-05-07 20:18:44 +0000

    Lagaði hvernig account og allar hans tengingar eru settar upp og kláraði að gera breyta persónupuulýsingum

---
+++ Migrations/20180507194714_newTablesUpdate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class newTablesUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "aspUserIdForShipping",
+                table: "shipingInfo",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "aspUserIdForCardInfo",
+                table: "cardInfo",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "aspUserIdForShipping",
+                table: "shipingInfo");
+
+            migrationBuilder.DropColumn(
+                name: "aspUserIdForCardInfo",
+                table: "cardInfo");
+        }
+    }
+}
