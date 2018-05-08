e982080277793aef86bad629357e57c6ff25f220
GunnlaugurG <34029342+GunnlaugurG@users.noreply.github.com>
2018-05-08 14:57:58 +0000

    new Migration

---
+++ Migrations/20180508145421_fixCart.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace BookCave.Migrations
+{
+    public partial class fixCart : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "orderComplete",
+                table: "carts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "orderComplete",
+                table: "carts");
+        }
+    }
+}
